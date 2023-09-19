8abef6b247a70329ac21be6097157d7cc73d7a58
David Dionisio <ddionisio@renegadeware.com>
2023-09-19 19:21:46 -0400

    revamped tutorial and lessons in overworld02 (Coriolis effect and ocean current stuff)

--- Assets/Scripts/Game/Overworld/OverworldSequence02.cs
+++ Assets/Scripts/Game/Overworld/OverworldSequence02.cs
@@ -10,34 +10,108 @@ public class OverworldSequence02 : OverworldSequenceBase {
     public AtmosphereAttributeBase wind;
     public AtmosphereAttributeBase temperature;
 
-    [Header("Dialogs")]
+    [Header("Illustrations")]
+    public AnimatorEnterExit windDirsIllustration;
+	public AnimatorEnterExit gulfStreamIllustration;
+
+	[Header("Dialogs")]
     public ModalDialogFlowIncremental dlgIntro;
-    public ModalDialogFlowIncremental dlgWind;
-    public ModalDialogFlowIncremental dlgWindTemp;
-    public ModalDialogFlowIncremental dlgIntroPost;
+    public ModalDialogFlowIncremental dlgCoriolis;
+    public ModalDialogFlowIncremental dlgWinds;
+	public ModalDialogFlowIncremental dlgOcean;
+	public ModalDialogFlowIncremental dlgGulfStream;
+	public ModalDialogFlowIncremental dlgIntroPost;
 
     [Header("Signal Invoke")]
     public SignalAtmosphereAttribute signalInvokeAtmosphereToggle;
+    public SignalSeasonData signalInvokeSeasonToggle;
+
+    private Coroutine mSeasonCycleRout;
 
     public override IEnumerator StartBegin() {
-
         yield return dlgIntro.Play();
-    }
+
+		signalInvokeSeasonToggle?.Invoke(GameData.instance.seasons[0]);
+
+		if(OverworldController.instance.overworldWindFX)
+			OverworldController.instance.overworldWindFX.Play();
+
+		signalInvokeAtmosphereToggle.Invoke(wind);
+
+        yield return new WaitForSeconds(2f);
+
+        yield return dlgCoriolis.Play();
+
+        if(windDirsIllustration) {
+            windDirsIllustration.Show();
+            windDirsIllustration.PlayEnter();
+		}
+
+		yield return new WaitForSeconds(2f);
+
+        yield return dlgWinds.Play();
+
+		if(windDirsIllustration) {
+            yield return windDirsIllustration.PlayExitWait();
+			windDirsIllustration.Hide();
+		}
+
+		signalInvokeAtmosphereToggle.Invoke(temperature);
+
+		yield return dlgOcean.Play();
+
+        if(gulfStreamIllustration) {
+            gulfStreamIllustration.Show();
+            gulfStreamIllustration.PlayEnter();
+		}
+
+        StartSeasonCycle();
+
+		yield return dlgGulfStream.Play();
+
+        StopSeasonCycle();
+
+		if(gulfStreamIllustration) {
+            yield return gulfStreamIllustration.PlayExitWait();
+			gulfStreamIllustration.Hide();			
+		}
+
+		signalInvokeAtmosphereToggle.Invoke(none);
+	}
 
     public override IEnumerator StartFinish() {
-        if(OverworldController.instance.overworldWindFX)
-            OverworldController.instance.overworldWindFX.Play();
-
-        signalInvokeAtmosphereToggle.Invoke(wind);
-
-        yield return dlgWind.Play();
-
-        signalInvokeAtmosphereToggle.Invoke(temperature);
-
-        yield return dlgWindTemp.Play();
-
-        signalInvokeAtmosphereToggle.Invoke(none);
-
         yield return dlgIntroPost.Play();
+    }
+
+    private void StartSeasonCycle() {
+        if(mSeasonCycleRout != null)
+            StopCoroutine(mSeasonCycleRout);
+
+        mSeasonCycleRout = StartCoroutine(DoSeasonCycle());
+	}
+
+    private void StopSeasonCycle() {
+        if(mSeasonCycleRout != null) {
+            StopCoroutine(mSeasonCycleRout);
+            mSeasonCycleRout = null;
+		}
+
+        signalInvokeSeasonToggle?.Invoke(GameData.instance.seasons[0]);
+	}
+
+    IEnumerator DoSeasonCycle() {
+        var wait = new WaitForSeconds(4f);
+
+        int curInd = 1;
+
+        while(true) {
+            signalInvokeSeasonToggle?.Invoke(GameData.instance.seasons[curInd]);
+
+            curInd++;
+            if(curInd == GameData.instance.seasons.Length)
+                curInd = 0;
+
+            yield return wait;
+        }
     }
 }
